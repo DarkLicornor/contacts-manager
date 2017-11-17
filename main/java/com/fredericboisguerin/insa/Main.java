6da8bdf47ce779b6784c807e9064ff86d4658ae8
Mme Leclerc-Istria Ombeline <leclerc-@insa-10221>
2017-11-17 10:18:42 +0100

    Test1 must validé

--- main/java/com/fredericboisguerin/insa/Main.java
+++ main/java/com/fredericboisguerin/insa/Main.java
@@ -10,16 +10,16 @@ public class Main {
         String exit = "false";
         
         while (exit == "false") {
-            System.out.println("What do you want to do ? (1/2/3)\n1) Add contact\n2)Find a contact\n3)Quit");
+            System.out.println("What do you want to do ? (1/2/3/4)\n1) Add contact\n2) Find a contact\n3) Print all contacts\n4) Quit");
             int choice = sc.nextInt();
             switch(choice) {
                 case 1:
                     System.out.println("Add contact : enter name");
-                    String name = sc.nextLine();
+                    String name = sc.next();
                     System.out.println("Enter email");
-                    String email = sc.nextLine();
+                    String email = sc.next();
                     System.out.println("Enter phone number");
-                    String phoneNumber = sc.nextLine();
+                    String phoneNumber = sc.next();
                     try {
                         cm.addContact(name, email, phoneNumber);
                     } catch (InvalidContactNameException e) {
@@ -30,12 +30,15 @@ public class Main {
                     break;
                 case 2:
                     System.out.println("Find a contact : enter name");
-                    String nameToFind = sc.nextLine();
+                    String nameToFind = sc.next();
                     cm.searchContactByName(nameToFind);
                     break;
                 case 3:
+                    cm.printAllContacts();
+                    break;
+                case 4:
                     System.out.println("Are you sure you want to quit ? (y/n)");
-                    String exitChoice = sc.nextLine();
+                    String exitChoice = sc.next();
                     if (exitChoice.equals("y")) {
                         exit = "true";
                         break;
